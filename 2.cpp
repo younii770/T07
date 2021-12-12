b0fe8e30ae5933897e7a31d6acc7c151e6ecbb2e
Younes Nosrati <77382993+younii770@users.noreply.github.com>
2021-12-12 15:11:46 +0330

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,41 +1,18 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+
 using namespace std;
-int binsearch(int low, int high, int x, int s[])
+
+int fibo(int n)
 {
-	int mid;
-	if (low > high)
-		return (-1);
-	else
-	{
-		mid = (low + high) / 2;
-		if (x == s[mid])
-			return mid;
-		else if (x < s[mid])
-			return binsearch(low, mid - 1, x, s);
-		else
-			return binsearch(mid + 1, high, x, s);
-	}
+	if (n <= 1)
+		return n;
+	return fibo(n - 1) + fibo(n - 2);
 }
+
 int main()
 {
-	int i, x, z, l = 0, h = 6, s[6];
-	cout << "Enter the 6-numbers: " << endl;
-	for (i = 0; i < 6; i++)
-	{
-		cout << "number " << i + 1 << " :";
-		cin >> s[i];
-	}
-	cout << "Enter the number Search:" << endl;
-	cin >> x;
-	z = binsearch(l, h, x, s);
-	if (z == (-1))
-		cout << "Not found";
-	else
-		cout << "The place is :" << z + 1;
-	
-	_getch();
-	return 0;
+	int n;
+		cout<<"Enter number = ";
+	cin >> n;
+	cout <<"Fibo = "<< fibo(n) <<endl;
 }
-
-
